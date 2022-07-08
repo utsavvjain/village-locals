43e8a3f7d4a078d183a1f25d3f001024a7370465
Utsav <utsavj502@gmail.com>
2022-07-08 12:03:17 +0530

    initial commit

---
+++ src/main/java/com/village/locals/village_locals/VillageLocalsApplication.java
@@ -0,0 +1,13 @@
+package com.village.locals.village_locals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillageLocalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillageLocalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/village/locals/village_locals/VillageLocalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.village.locals.village_locals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillageLocalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
